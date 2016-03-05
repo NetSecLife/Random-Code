288e1ea313f60bf65738d2ce954bb3d46a7b8e4d
NetSecLife <netseclife@netseclife.com>
2016-03-05 13:57:21 +1100

    ABC News Headline Web Scraper
    Scrapes the ABC News Australia website for its headlines

---
+++ abc-news-scraper.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+def scrape_headlines(soup):
+    #Constrains to the headline section
+    headlines = soup.find("ol")
+    #For loop through headlines
+    for heading in headlines:
+        #Get rid of whitespace, then collect the data
+        if heading == "\n":
+            continue
+        headline = heading.find("a").getText()
+        description = heading.find("p").getText()
+        for z in heading.find_all('a', href=True):
+            url = (z['href'])
+        #Pretty output
+        Headline = "Headline: " + headline + "."
+        Description = "Description: " + description
+        URL = "URL: http://www.abc.net.au" + url
+        print(Headline)
+        print(Description)
+        print(URL + "\n")
+
+def main():
+    #Open website and assign the source code to html
+    with urllib.request.urlopen('http://www.abc.net.au/news/') as response:
+        html = response.read()
+    #Setup soup variable for harvesting
+    soup = BeautifulSoup(html, 'html.parser')
+    scrape_headlines(soup)
+
+main()
+
+#Logic flow.
+#Open site with urllib
+#Assign site code to html variable
+#Soup the html
+#Constrain soup to the first <ol> tags
+#For loop
+#Gather the headline
+#Gather the description
+#Gather the url
+#Add global abc site url to url variable
+#Put together and output
